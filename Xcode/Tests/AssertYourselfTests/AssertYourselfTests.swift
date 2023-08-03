bf246f5121fa545e98a6d047c97baf361082928e
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-03 22:30:40 +0200

    Finish the "Assert Yourself" chapter

--- Xcode/Tests/AssertYourselfTests/AssertYourselfTests.swift
+++ Xcode/Tests/AssertYourselfTests/AssertYourselfTests.swift
@@ -13,4 +13,71 @@ final class AssertYourselfTests: XCTestCase {
     func test_fail() {
         XCTFail()
     }
+
+    func test_fail_withSimpleMessage() {
+        XCTFail("We have a problem")
+    }
+
+    func test_fail_withInterpolatedMessage() {
+        let theAnswer = 42
+        XCTFail("The Answer to the Great Question is \(theAnswer)")
+    }
+
+    func test_assertTrue() {
+        let success = false
+        XCTAssertTrue(success)
+    }
+
+    func test_assertNil() {
+        let optionalValue: Int? = 123
+        XCTAssertNil(optionalValue)
+    }
+
+    struct SimpleStruct {
+        let x: Int
+        let y: Int
+    }
+
+    func test_assertNil_withSimpleStruct() {
+        let optionalValue: SimpleStruct? = .init(x: 1, y: 2)
+        XCTAssertNil(optionalValue)
+    }
+
+    struct SimpleStructWithDescription: CustomStringConvertible {
+        let x: Int
+        let y: Int
+
+        var description: String { "\(x), \(y)" }
+    }
+
+    func test_assertNil_withSelfDescribingType() {
+        let optionalValue: SimpleStructWithDescription? = .init(x: 1, y: 2)
+        XCTAssertNil(optionalValue)
+    }
+
+    func test_assertEqual() {
+        let actual = "actual"
+        XCTAssertEqual(actual, "expected")
+    }
+
+    func test_assertEqual_withOptional() {
+        let result: String? = "foo"
+        XCTAssertEqual(result, "bar")
+    }
+
+    func test_floatingPointDanger() {
+        let result = 0.1 + 0.2
+        XCTAssertEqual(result, 0.3)
+    }
+
+    func test_floatingPointFixed() {
+        let result = 0.1 + 0.2
+        XCTAssertEqual(result, 0.3, accuracy: 0.0001)
+    }
+
+    func test_messageOverkill() {
+        let actual = "actual"
+        XCTAssertEqual(actual, "expected",
+                       "Expected \"expected\" but got \"\(actual)\"")
+    }
 }

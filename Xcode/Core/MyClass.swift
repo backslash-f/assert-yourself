f829ef02d54b8f83c7ec175b9c5d35d55677e3bb
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-04 17:49:22 +0200

    Added MyClass

---
+++ Xcode/Core/MyClass.swift
@@ -0,0 +1,18 @@
+//
+//  MyClass.swift
+//  AssertYourself
+//
+//  Created by Fernando Fernandes on 04.08.23.
+//
+
+import Foundation
+
+class MyClass {
+    func methodOne() {
+        print(">> methodOne")
+    }
+
+    func methodTwo() {
+        print(">> methodTwo")
+    }
+}

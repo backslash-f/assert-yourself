629c54b4e8d6281878bac8ac8b6396097f71f3f2
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-04 18:51:12 +0200

    Finish the Life Cycle chapter

--- Xcode/Tests/LifeCycleTests/LifeCycleTests.swift
+++ Xcode/Tests/LifeCycleTests/LifeCycleTests.swift
@@ -7,4 +7,31 @@
 
 import XCTest
 
-final class LifeCycleTests: XCTestCase {}
+@testable import AssertYourself
+
+final class LifeCycleTests: XCTestCase {
+
+    private var sut: MyClass!
+
+    override func setUp() {
+        super.setUp()
+        sut = MyClass()
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func test_methodOne() {
+        sut.methodOne()
+        
+        // Normally, assert something
+    }
+
+    func test_methodTwo() {
+        sut.methodTwo()
+
+        // Normally, assert something
+    }
+}

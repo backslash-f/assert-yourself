71aac8a2d881d2b1eb028ce34be2236a3c9dc243
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-14 11:42:53 +0200

    Create AppLaunch target / test plan

---
+++ Xcode/Tests/AppLaunchTests/AppLaunchTests.swift
@@ -0,0 +1,15 @@
+//
+//  AppLaunchTests.swift
+//  AppLaunchTests
+//
+//  Created by Fernando Fernandes on 14.08.23.
+//
+
+import XCTest
+
+final class AppLaunchTests: XCTestCase {
+
+    func test_empty() {
+        XCTFail()
+    }
+}

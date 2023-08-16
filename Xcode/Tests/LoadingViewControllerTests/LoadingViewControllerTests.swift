01b7941f2ba1b1115cccc6dd0e064e275cd84de8
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-16 15:45:25 +0200

    Finish the Storyboard part

--- Xcode/Tests/LoadingViewControllerTests/LoadingViewControllerTests.swift
+++ Xcode/Tests/LoadingViewControllerTests/LoadingViewControllerTests.swift
@@ -7,8 +7,16 @@
 
 import XCTest
 
+@testable import AssertYourself
+
 final class LoadingViewControllerTests: XCTestCase {
-    func test_zero() {
-        XCTFail()
+    func test_loading() {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let sut: StoryboardBasedViewController = sb.instantiateViewController(
+            identifier: String(describing: StoryboardBasedViewController.self)
+        )
+        sut.loadViewIfNeeded()
+        
+        XCTAssertNotNil(sut.label)
     }
 }
---
+++ Xcode/ViewController/StoryboardBasedViewController.swift
@@ -0,0 +1,12 @@
+//
+//  StoryboardBasedViewController.swift
+//  AssertYourself
+//
+//  Created by Fernando Fernandes on 16.08.23.
+//
+
+import UIKit
+
+class StoryboardBasedViewController: UIViewController {
+    @IBOutlet var label: UILabel!
+}

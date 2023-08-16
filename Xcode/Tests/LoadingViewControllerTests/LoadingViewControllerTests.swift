c39a6ab35ac8c3634de04d5f34b70d78cfc6c5a0
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-16 15:24:01 +0200

    Create LoadingViewControllers test/test plan

---
+++ Xcode/Tests/LoadingViewControllerTests/LoadingViewControllerTests.swift
@@ -0,0 +1,14 @@
+//
+//  LoadingViewControllerTests.swift
+//  LoadingViewControllerTests
+//
+//  Created by Fernando Fernandes on 16.08.23.
+//
+
+import XCTest
+
+final class LoadingViewControllerTests: XCTestCase {
+    func test_zero() {
+        XCTFail()
+    }
+}

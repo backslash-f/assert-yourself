1117df071f1aa7f7c75742840671e600839636f1
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-17 11:48:08 +0200

    Add HardDependencies test/test plan

---
+++ Xcode/Tests/HardDependenciesTests/HardDependenciesTests.swift
@@ -0,0 +1,17 @@
+//
+//  HardDependenciesTests.swift
+//  HardDependenciesTests
+//
+//  Created by Fernando Fernandes on 17.08.23.
+//
+
+import XCTest
+
+@testable import AssertYourself
+
+final class HardDependenciesTests: XCTestCase {
+
+    func test_zero() throws {
+        XCTFail()
+    }
+}

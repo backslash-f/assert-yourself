986706050362f8ed8e55ce9f7b24c78dd0cfd14a
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-17 13:08:29 +0200

    Create Analytics storyboards/view controllers

---
+++ Xcode/ViewController/Analytics/ClosurePropertyViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ClosurePropertyViewController.swift
+//  AssertYourself
+//
+//  Created by Fernando Fernandes on 17.08.23.
+//
+
+import UIKit
+
+class ClosurePropertyViewController: UIViewController {}
---
+++ Xcode/ViewController/Analytics/InstancePropertyViewController.swift
@@ -0,0 +1,10 @@
+//
+//  InstancePropertyViewController.swift
+//  AssertYourself
+//
+//  Created by Fernando Fernandes on 17.08.23.
+//
+
+import UIKit
+
+class InstancePropertyViewController: UIViewController {}

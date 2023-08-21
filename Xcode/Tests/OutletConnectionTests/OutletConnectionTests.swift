17ced49942188146f3a9147f8d1278327459c54e
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-21 09:46:43 +0200

    Add OutletConnections test/testplan

---
+++ Xcode/Tests/OutletConnectionTests/OutletConnectionTests.swift
@@ -0,0 +1,15 @@
+//
+//  OutletConnectionTests.swift
+//  OutletConnectionTests
+//
+//  Created by Fernando Fernandes on 21.08.23.
+//
+
+import XCTest
+
+final class OutletConnectionTests: XCTestCase {
+
+    func test_zero() {
+        XCTFail()
+    }
+}

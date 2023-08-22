4681bfb56bb3c98ce7ca28f9709094a2dcd9243e
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-22 09:37:28 +0200

    Create NetworkRequest test/test plan

---
+++ Xcode/Tests/NetworkRequestTests/NetworkRequestTests.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkRequestTests.swift
+//  NetworkRequestTests
+//
+//  Created by Fernando Fernandes on 22.08.23.
+//
+
+import XCTest
+
+final class NetworkRequestTests: XCTestCase {
+
+    func test_zero() {
+        XCTFail()
+    }
+}

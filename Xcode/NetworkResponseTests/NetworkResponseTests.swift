14de5f2d272d3f3664aa01318ba9ada7bdb2a673
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-26 19:41:27 +0200

    Add NetworkResponse test/test plan

---
+++ Xcode/NetworkResponseTests/NetworkResponseTests.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkResponseTests.swift
+//  NetworkResponseTests
+//
+//  Created by Fernando Fernandes on 26.08.23.
+//
+
+import XCTest
+
+final class NetworkResponseTests: XCTestCase {
+    func test_zero() {
+        XCTFail()
+    }
+}

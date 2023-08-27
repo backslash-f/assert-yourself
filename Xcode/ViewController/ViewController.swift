bc0b9cdc7e450edc533d4f713158421aba8a2f2c
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-27 21:55:08 +0200

    Rename tests

--- Xcode/ViewController/ViewController.swift
+++ Xcode/ViewController/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
 
     private(set) var results: [SearchResult] = [] {
         didSet {
+            print(">> Number of results: \(results.count)")
             print(">> Results: \(results)")
         }
     }

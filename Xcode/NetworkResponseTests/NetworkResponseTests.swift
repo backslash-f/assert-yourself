fb7c4145ea5203bd892a78230853e08ddae52bc5
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-08-27 21:56:08 +0200

    Finish happy path testing

--- Xcode/NetworkResponseTests/NetworkResponseTests.swift
+++ Xcode/NetworkResponseTests/NetworkResponseTests.swift
@@ -7,8 +7,55 @@
 
 import XCTest
 
-final class NetworkResponseTests: XCTestCase {
-    func test_zero() {
-        XCTFail()
+@testable import AssertYourself
+
+final class ViewControllerTests: XCTestCase {
+
+    @MainActor
+    func test_searchForBookNetworkCall_withSuccessResponse_shouldSaveDataInResults() async {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let sut: ViewController = sb.instantiateViewController(
+            identifier: String(describing: ViewController.self)
+        )
+
+        let spyURLSession = SpyURLSession(
+            data: jsonData(),
+            responseStatusCode: 200
+        )
+        sut.session = spyURLSession
+        sut.loadViewIfNeeded()
+
+        await sut.searchForBook(terms: sut.hardcodedSearchTerm)
+
+        XCTAssertEqual(sut.results, [
+            SearchResult(
+                artistName: "Artist",
+                trackName: "Track",
+                averageUserRating: 2.5,
+                genres: ["Foo", "Bar"]
+            )
+        ])
     }
+}
+
+// MARK: - Private
+
+private func jsonData() -> Data {
+    """
+    {
+        "results":
+        [
+            {
+                "artistName": "Artist",
+                "trackName": "Track",
+                "averageUserRating": 2.5,
+                "genres":
+                [
+                    "Foo",
+                    "Bar"
+                ]
+            }
+        ]
+    }
+    """.data(using: .utf8)!
 }

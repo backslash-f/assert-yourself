b528d0468bdb9362f9ceffc4ddd1649473c7a765
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-10-16 09:39:09 +0200

    Create the ButtonTapsTest test/testplan
     - Update to Xcode 15.

---
+++ Xcode/Tests/ButtonTapsTests/ButtonTapsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonTapsTests.swift
+//  ButtonTapsTests
+//
+//  Created by Fernando Fernandes on 16.10.23.
+//
+
+import XCTest
+
+@testable import AssertYourself
+
+final class ButonTapsTests: XCTestCase {
+
+    func test_zero() {
+        XCTFail("NHANHA")
+    }
+}

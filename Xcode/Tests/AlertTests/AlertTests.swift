5bd6996fadb6d8431c031c92e7f3a9bc3fb68511
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-10-30 16:31:23 +0100

    Add test_tappingButton_shouldShowAlert
     - Fix ViewControllerPresentationSpy linking.

--- Xcode/Tests/AlertTests/AlertTests.swift
+++ Xcode/Tests/AlertTests/AlertTests.swift
@@ -5,11 +5,51 @@
 //  Created by Fernando Fernandes on 30.10.23.
 //
 
+import ViewControllerPresentationSpy
 import XCTest
+
+@testable import AssertYourself
 
 final class AlertTests: XCTestCase {
 
-    func testZero() throws {
-        XCTFail()
+    private var alertVerifier: AlertVerifier!
+
+    @MainActor
+    override func setUp() {
+        super.setUp()
+        alertVerifier = AlertVerifier()
+    }
+
+    override func tearDown() {
+        alertVerifier = nil
+        super.tearDown()
+    }
+
+    @MainActor
+    func test_tappingButton_shouldShowAlert() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let sut: ViewController = storyboard.instantiateViewController(
+            withIdentifier: String(describing: ViewController.self)
+        ) as! ViewController
+        sut.loadViewIfNeeded()
+
+        tap(sut.alertButton)
+
+        alertVerifier.verify(
+            title: "Do the Thing?",
+            message: "Let us know if you want to do the thing.",
+            animated: true,
+            actions: [
+                .cancel("Cancel"),
+                .default("OK")
+            ],
+            presentingViewController: sut
+        )
+
+        XCTAssertEqual(
+            alertVerifier.preferredAction?.title,
+            "OK",
+            "preferred action"
+        )
     }
 }

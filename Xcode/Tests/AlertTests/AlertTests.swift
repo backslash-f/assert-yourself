5b2a92cbd7fd93af2de761d6bd6b166eb9439115
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-10-30 16:03:01 +0100

    Create AlertTests test/plan

---
+++ Xcode/Tests/AlertTests/AlertTests.swift
@@ -0,0 +1,15 @@
+//
+//  AlertTests.swift
+//  AlertTests
+//
+//  Created by Fernando Fernandes on 30.10.23.
+//
+
+import XCTest
+
+final class AlertTests: XCTestCase {
+
+    func testZero() throws {
+        XCTFail()
+    }
+}

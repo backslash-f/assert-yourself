c139113707bb11c73f5c40fa7521b723a48ec4e2
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-11-01 09:20:07 +0100

    Create navigation test/testplan

---
+++ Xcode/NavigationTests/NavigationTests.swift
@@ -0,0 +1,15 @@
+//
+//  NavigationTests.swift
+//  NavigationTests
+//
+//  Created by Fernando Fernandes on 01.11.23.
+//
+
+import XCTest
+
+final class NavigationTests: XCTestCase {
+
+    func testZero() throws {
+        XCTFail()
+    }
+}

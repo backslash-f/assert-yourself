eb4a966c7b9bb9227878c85ed3c12442485eb925
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-11-02 07:28:58 +0100

    Create UserDefaults test/plan

---
+++ Xcode/Tests/UserDefaultsTests/UserDefaultsTests.swift
@@ -0,0 +1,15 @@
+//
+//  UserDefaultsTests.swift
+//  UserDefaultsTests
+//
+//  Created by Fernando Fernandes on 02.11.23.
+//
+
+import XCTest
+
+final class UserDefaultsTests: XCTestCase {
+
+    func testZero() {
+        XCTFail()
+    }
+}

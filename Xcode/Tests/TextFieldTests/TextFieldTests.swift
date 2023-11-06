ef8cdc6f23c3c713186f3bd1bd52b15f41a5993b
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-11-06 09:50:15 +0100

    Create the TextField test/plan

---
+++ Xcode/Tests/TextFieldTests/TextFieldTests.swift
@@ -0,0 +1,15 @@
+//
+//  TextFieldTests.swift
+//  TextFieldTests
+//
+//  Created by Fernando Fernandes on 06.11.23.
+//
+
+import XCTest
+
+final class TextFieldTests: XCTestCase {
+
+    func testZero() throws {
+        XCTFail()
+    }
+}

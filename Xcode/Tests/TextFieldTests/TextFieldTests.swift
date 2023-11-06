2e7e5cb1130b90c96526293e1d05bf78d4ab1a8b
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-11-06 11:35:07 +0100

    Test IBOutlets

--- Xcode/Tests/TextFieldTests/TextFieldTests.swift
+++ Xcode/Tests/TextFieldTests/TextFieldTests.swift
@@ -7,9 +7,28 @@
 
 import XCTest
 
+@testable import AssertYourself
+
 final class TextFieldTests: XCTestCase {
 
-    func testZero() throws {
-        XCTFail("test_zero")
+    private var sut: ViewController!
+
+    @MainActor
+    override func setUp() {
+        super.setUp()
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let identifier = String(describing: ViewController.self)
+        sut = sb.instantiateViewController(identifier: identifier)
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func test_outlets_shouldBeConnected() throws {
+        sut.loadViewIfNeeded()
+        XCTAssertNotNil(sut.usernameField, "usernameField")
+        XCTAssertNotNil(sut.passwordField, "passwordField")
     }
 }

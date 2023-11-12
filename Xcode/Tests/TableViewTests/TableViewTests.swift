0d75d3c820b2b408ed87cf0f776f6fc5dc034134
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-11-12 21:22:48 +0100

    Add TableView tests/plan

---
+++ Xcode/Tests/TableViewTests/TableViewTests.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewTests.swift
+//  TableViewTests
+//
+//  Created by Fernando Fernandes on 12.11.23.
+//
+
+import XCTest
+
+final class TableViewTests: XCTestCase {
+
+    func testZero() throws {
+        XCTFail("Test Zero")
+    }
+}

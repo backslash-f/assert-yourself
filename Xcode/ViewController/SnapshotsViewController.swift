2768bd5be15e36f797d8612a5b1a4f73a2fbe977
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-11-13 00:30:31 +0100

    Create the SnapshotsViewController

---
+++ Xcode/ViewController/SnapshotsViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SnapshotsViewController.swift
+//  AssertYourself
+//
+//  Created by Fernando Fernandes on 12.11.23.
+//
+
+import Foundation
+import UIKit
+
+class SnapshotsViewController: UIViewController {}

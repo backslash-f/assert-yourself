746fa462377062c8c4c8c835830bcc2aaf8e1729
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-11-12 22:43:14 +0100

    Create the Snapshot test/plan

---
+++ Xcode/Tests/SnapshotTests/SnapshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotTests.swift
+//  SnapshotTests
+//
+//  Created by Fernando Fernandes on 12.11.23.
+//
+
+import XCTest
+
+@testable import AssertYourself
+
+final class SnapshotTests: XCTestCase {
+
+    func test_zero() throws {
+        XCTFail("Test zero")
+    }
+}

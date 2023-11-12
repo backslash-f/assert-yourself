58e5fcdb761685683ce1b4d9e495eede3e3c68a4
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-11-12 22:01:05 +0100

    Finish adding table view tests

--- Xcode/Tests/Helpers/TestHelpers.swift
+++ Xcode/Tests/Helpers/TestHelpers.swift
@@ -95,3 +95,20 @@ func putInViewHierarchy(_ vc: UIViewController) {
     let window = UIWindow()
     window.addSubview(vc.view)
 }
+
+func numberOfRows(in tableView: UITableView, section: Int = 0) -> Int? {
+    tableView.dataSource?.tableView(
+        tableView,
+        numberOfRowsInSection: section
+    )
+}
+
+func cellForRow(in tableView: UITableView, row: Int, section: Int = 0) -> UITableViewCell? {
+    let indexPath = IndexPath(row: row, section: section)
+    return tableView.dataSource?.tableView(tableView, cellForRowAt: indexPath)
+}
+
+func didSelectRow(in tableView: UITableView, row: Int, section: Int = 0) {
+    let indexPath = IndexPath(row: row, section: section)
+    tableView.delegate?.tableView?(tableView, didSelectRowAt: indexPath)
+}
--- Xcode/Tests/TableViewTests/TableViewTests.swift
+++ Xcode/Tests/TableViewTests/TableViewTests.swift
@@ -7,9 +7,52 @@
 
 import XCTest
 
+@testable import AssertYourself
+
 final class TableViewTests: XCTestCase {
 
-    func testZero() throws {
-        XCTFail("Test Zero")
+    private var sut: TableViewController!
+
+    @MainActor
+    override func setUp() {
+        super.setUp()
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let identifier = String(describing: TableViewController.self)
+        sut = sb.instantiateViewController(identifier: identifier)
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func test_tableViewDelegates_shouldBeConnected() throws {
+        XCTAssertNotNil(sut.tableView.dataSource, "dataSource")
+        XCTAssertNotNil(sut.tableView.delegate, "delegate")
+    }
+
+    func test_numberOfRows_shouldBe3() throws {
+        XCTAssertEqual(numberOfRows(in: sut.tableView), 3)
+    }
+
+    func test_cellForRowAt_withRow0_shouldSetCellLabelToOne() {
+        let cell = cellForRow(in: sut.tableView, row: 0)
+        XCTAssertEqual(cell?.textLabel?.text, "One")
+    }
+
+    func test_cellForRowAt_withRow1_shouldSetCellLabelToTwo() {
+        let cell = cellForRow(in: sut.tableView, row: 1)
+        XCTAssertEqual(cell?.textLabel?.text, "Two")
+    }
+
+    func test_cellForRow_withRow2_shouldSetCellLabelToThree() {
+        let cell = cellForRow(in: sut.tableView, row: 2)
+        XCTAssertEqual(cell?.textLabel?.text, "Three")
+    }
+
+    func test_didSelectRow_withRow1() {
+        didSelectRow(in: sut.tableView, row: 1)
+        // Normally, assert something
     }
 }

e747069dd6cedc0a4f9be7276c20a230932bac42
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-11-18 22:19:10 +0100

    Finish migrating strings to the view model

--- Xcode/ViewController/ChangePassword/ChangePasswordViewModel.swift
+++ Xcode/ViewController/ChangePassword/ChangePasswordViewModel.swift
@@ -8,23 +8,42 @@
 import Foundation
 
 struct ChangePasswordViewModel {
+    let title: String
     let okButtonLabel: String
-    let enterNewPasswordMessage: String
-    let newPasswordTooShortMessage: String
-    let confirmationPasswordDoesNotMatchMessage: String
+    let submitButtonLabel: String
     let successMessage: String
 
-    init(okButtonLabel: String = "OK",
+    let oldPasswordPlaceholder: String
+
+    let enterNewPasswordMessage: String
+    let newPasswordPlaceholder: String
+    let newPasswordTooShortMessage: String
+
+    let confirmPasswordPlaceholder: String
+    let confirmationPasswordDoesNotMatchMessage: String
+
+    init(title: String = "Change Password",
+         okButtonLabel: String = "OK",
+         submitButtonLabel: String = "Submit",
+         successMessage: String = "Your password has been successfully changed.",
+         oldPasswordPlaceholder: String = "Current Password",
          enterNewPasswordMessage: String = "Please enter a new password.",
+         newPasswordPlaceholder: String = "New Password",
          newPasswordTooShortMessage: String = "The new password should have at least 6 characters.",
+         confirmPasswordPlaceholder: String = "Confirm New Password",
          confirmationPasswordDoesNotMatchMessage: String = """
         The new password and the confirmation password don’t match. Please try again.
-        """,
-         successMessage: String = "Your password has been successfully changed.") {
+        """
+    ) {
+        self.title = title
         self.okButtonLabel = okButtonLabel
-        self.enterNewPasswordMessage = enterNewPasswordMessage
-        self.newPasswordTooShortMessage = newPasswordTooShortMessage
-        self.confirmationPasswordDoesNotMatchMessage = confirmationPasswordDoesNotMatchMessage
+        self.submitButtonLabel = submitButtonLabel
         self.successMessage = successMessage
+        self.oldPasswordPlaceholder = oldPasswordPlaceholder
+        self.enterNewPasswordMessage = enterNewPasswordMessage
+        self.newPasswordPlaceholder = newPasswordPlaceholder
+        self.newPasswordTooShortMessage = newPasswordTooShortMessage
+        self.confirmPasswordPlaceholder = confirmPasswordPlaceholder
+        self.confirmationPasswordDoesNotMatchMessage = confirmationPasswordDoesNotMatchMessage
     }
 }

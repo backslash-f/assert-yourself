75b2ff8462156cf0adb77c80b569d8763404d54b
Fernando Fernandes <mfernandes.fernando@gmail.com>
2023-11-19 15:33:34 +0100

    Add second test

--- Xcode/TDD/Sources/TDD/Greeter.swift
+++ Xcode/TDD/Sources/TDD/Greeter.swift
@@ -5,6 +5,18 @@ struct Greeter {
     }
 
     func greet(time: Date) -> String {
+        if hour(for: time) == 12 {
+            return "Good afternoon."
+        }
         return "Good morning."
+    }
+}
+
+// MARK: - Private
+
+private extension Greeter {
+    func hour(for time: Date) -> Int {
+        let components = Calendar.current.dateComponents([.hour], from: time)
+        return components.hour ?? 0
     }
 }
--- Xcode/TDD/Tests/TDDTests/GreeterTests.swift
+++ Xcode/TDD/Tests/TDDTests/GreeterTests.swift
@@ -2,7 +2,19 @@ import XCTest
 
 @testable import TDD
 
-final class GreeterTests: XCTestCase {
+final class GreeterWithoutNameTests: XCTestCase {
+
+    private var sut: Greeter!
+
+    override func setUp() {
+        super.setUp()
+        sut = Greeter(name: "")
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
 
     func test_greet_with1159am_shouldSayGoodMorning() throws {
         guard let time = date(hour: 11, minute: 59) else {
@@ -10,9 +22,19 @@ final class GreeterTests: XCTestCase {
             return
         }
 
-        let sut = Greeter(name: "")
         let result = sut.greet(time: time)
 
         XCTAssertEqual(result, "Good morning.")
+    }
+
+    func test_greet_with1200pm_shouldSayGoodAfternoon() {
+        guard let time = date(hour: 12, minute: 00) else {
+            XCTFail("Could not create a valid date")
+            return
+        }
+
+        let result = sut.greet(time: time)
+
+        XCTAssertEqual(result, "Good afternoon.")
     }
 }
